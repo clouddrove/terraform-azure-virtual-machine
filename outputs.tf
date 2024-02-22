379ea5789461581339d49eec00e57b237ebf75ad
Rupal Sharma <152851038+Rupalgw@users.noreply.github.com>
2024-02-22 15:29:33 +0530

    fix:change in count condition (#53)

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,40 @@
 output "network_interface_id" {
-  value       = azurerm_network_interface.default[*].id
+  value       = try(azurerm_network_interface.default[*].id, null)
   description = "The ID of the Network Interface."
 }
 
 output "network_interface_private_ip_addresses" {
-  value       = azurerm_network_interface.default[*].private_ip_addresses
+  value       = try(azurerm_network_interface.default[*].private_ip_addresses, null)
   description = "The private IP addresses of the network interface."
 }
 
 output "availability_set_id" {
-  value       = azurerm_availability_set.default[*].id
+  value       = try(azurerm_availability_set.default[*].id, null)
   description = "The ID of the Availability Set."
 }
 
 output "public_ip_id" {
-  value       = azurerm_public_ip.default[*].id
+  value       = try(azurerm_public_ip.default[*].id, null)
   description = "The Public IP ID."
 }
 
 output "public_ip_address" {
-  value       = azurerm_public_ip.default[*].ip_address
+  value       = try(azurerm_public_ip.default[*].ip_address, null)
   description = "The IP address value that was allocated."
 }
 
 output "linux_virtual_machine_id" {
-  value       = azurerm_linux_virtual_machine.default[*].id
+  value       = try(azurerm_linux_virtual_machine.default[*].id, null)
   description = "The ID of the Linux Virtual Machine."
 }
 
 output "windows_virtual_machine_id" {
-  value       = azurerm_windows_virtual_machine.win_vm[*].id
+  value       = try(azurerm_windows_virtual_machine.win_vm[*].id, null)
   description = "The ID of the Windows Virtual Machine."
 }
 
 output "network_interface_sg_association_id" {
-  value       = azurerm_network_interface_security_group_association.default[*].id
+  value       = try(azurerm_network_interface_security_group_association.default[*].id, null)
   description = "The (Terraform specific) ID of the Association between the Network Interface and the Network Interface."
 }
 
@@ -44,11 +44,11 @@ output "tags" {
 }
 
 output "disk_encryption_set-id" {
-  value = azurerm_disk_encryption_set.example[*].id
+  value = try(azurerm_disk_encryption_set.example[*].id, null)
 }
 
 output "key_id" {
-  value       = azurerm_key_vault_key.example[*].id
+  value       = try(azurerm_key_vault_key.example[*].id, null)
   description = "Id of key that is to be used for encrypting "
 }
 
