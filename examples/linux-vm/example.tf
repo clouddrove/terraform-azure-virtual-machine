b77d51a0732143e95bf8521e9eba88b36cca74bf
Maharshi Bhatnagar <159241848+NotMaharshi@users.noreply.github.com>
2024-10-15 17:15:12 +0530

    fix: Update azurerm version >=3.112.0 (#69)
    * fix:renamed _examples to examples and referenced it
    
    * fix:added provider block and fixed formatting
    
    * fix:fixed provider block linux-vm example
    
    * fix:updated tag for log analytics in linux vm example
    
    * fix: Deprecations
    
    * fix: CODEOWNERS
    
    * update codeowners
    
    * fix: CODEOWNERS
    
    ---------
    
    Co-authored-by: Vishal Sharma <vishal.sharma@clouddrove.com>

--- examples/linux-vm/example.tf
+++ examples/linux-vm/example.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+  subscription_id = "000000-11111-1223-XXX-XXXXXXXXXXXX"
+}
+provider "azurerm" {
+  features {}
+  alias           = "peer"
+  subscription_id = "000000-11111-1223-XXX-XXXXXXXXXXXX"
 }
 
 data "azurerm_client_config" "current_client_config" {}
@@ -11,10 +17,10 @@ data "azurerm_client_config" "current_client_config" {}
 module "resource_group" {
   source      = "clouddrove/resource-group/azure"
   version     = "1.0.2"
-  name        = "vm-windows-1"
+  name        = "vm"
   environment = "test"
   label_order = ["name", "environment"]
-  location    = "Canada Central"
+  location    = "Norway East"
 }
 
 ##-----------------------------------------------------------------------------
@@ -61,57 +67,94 @@ module "subnet" {
 ## security-group module call.
 ##-----------------------------------------------------------------------------
 module "security_group" {
-  source  = "clouddrove/network-security-group/azure"
-  version = "1.0.4"
-  ## Tags
-  name        = "app"
-  environment = "test"
-  label_order = ["name", "environment"]
-  ## Security Group
+  source                  = "clouddrove/network-security-group/azure"
+  version                 = "1.0.4"
+  name                    = "app"
+  environment             = "test"
   resource_group_name     = module.resource_group.resource_group_name
   resource_group_location = module.resource_group.resource_group_location
   subnet_ids              = module.subnet.default_subnet_id
-  ##Security Group rule for Custom port.
   inbound_rules = [
     {
-      name                       = "rdp"
+      name                       = "ssh"
       priority                   = 101
       access                     = "Allow"
       protocol                   = "Tcp"
       source_address_prefix      = "0.0.0.0/0"
       source_port_range          = "*"
       destination_address_prefix = "0.0.0.0/0"
-      destination_port_range     = "3389"
-      description                = "allow rdp port"
-    },
-    {
-      name                       = "http"
-      priority                   = 102
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "0.0.0.0/0"
-      source_port_range          = "*"
-      destination_address_prefix = "0.0.0.0/0"
-      destination_port_range     = "443"
-      description                = "allow https port"
+      destination_port_range     = "22"
+      description                = "ssh allowed port"
     }
   ]
 
 }
 
 ##-----------------------------------------------------------------------------
-## windows virtual-machine module call.
+## key-vault module call for disc encryption of virtual machine with cmk.
+#-----------------------------------------------------------------------------
+module "key_vault" {
+  source = "clouddrove/key-vault/azure"
+  providers = {
+    azurerm.dns_sub  = azurerm.peer, #change this to other alias if dns hosted in other subscription.
+    azurerm.main_sub = azurerm
+  }
+  version                     = "1.2.0"
+  name                        = "vae59d6058"
+  environment                 = "test"
+  label_order                 = ["name", "environment", ]
+  resource_group_name         = module.resource_group.resource_group_name
+  location                    = module.resource_group.resource_group_location
+  admin_objects_ids           = [data.azurerm_client_config.current_client_config.object_id]
+  virtual_network_id          = module.vnet.vnet_id
+  subnet_id                   = module.subnet.default_subnet_id[0]
+  enable_rbac_authorization   = true
+  enabled_for_disk_encryption = false
+  #private endpoint
+  enable_private_endpoint = false
+  network_acls            = null
+  ########Following to be uncommnented only when using DNS Zone from different subscription along with existing DNS zone.
+
+  # diff_sub                                      = true
+  # alias                                         = ""
+  # alias_sub                                     = ""
+
+  #########Following to be uncommmented when using DNS zone from different resource group or different subscription.
+  # existing_private_dns_zone                     = ""
+  # existing_private_dns_zone_resource_group_name = ""
+
+  #### enable diagnostic setting
+  diagnostic_setting_enable  = false
+  log_analytics_workspace_id = module.log-analytics.workspace_id ## when diagnostic_setting_enable enable,  add log analytics workspace id
+}
+
+##-----------------------------------------------------------------------------
+## log-analytics module call for diagnosis setting 
+#-----------------------------------------------------------------------------
+module "log-analytics" {
+  source                           = "clouddrove/log-analytics/azure"
+  version                          = "1.1.0"
+  name                             = "app"
+  environment                      = "test"
+  label_order                      = ["name", "environment"]
+  create_log_analytics_workspace   = true
+  log_analytics_workspace_sku      = "PerGB2018"
+  resource_group_name              = module.resource_group.resource_group_name
+  log_analytics_workspace_location = module.resource_group.resource_group_location
+  log_analytics_workspace_id       = module.log-analytics.workspace_id
+}
+
+##-----------------------------------------------------------------------------
+## linux virtual-machine module call.
 ##-----------------------------------------------------------------------------
 module "virtual-machine" {
   source              = "../../"
+  depends_on          = [module.key_vault]
   name                = "app"
   environment         = "test"
-  is_vm_windows       = true
-  enabled             = true
-  machine_count       = 1
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
-  disk_size_gb        = 128
+  is_vm_linux         = true
   user_object_id = {
     "user1" = {
       role_definition_name = "Virtual Machine Administrator Login"
@@ -127,32 +170,39 @@ module "virtual-machine" {
   ## Public IP
   public_ip_enabled = true
   ## Virtual Machine
-  computer_name   = "app-win-comp"
   vm_size         = "Standard_B1s"
-  admin_username  = "azureadmin"
-  admin_password  = "Password@123"
-  image_publisher = "MicrosoftWindowsServer"
-  image_offer     = "WindowsServer"
-  image_sku       = "2019-datacenter"
+  public_key      = "ssh-rsa AAAA"
+  admin_username  = "ubuntu"
+  caching         = "ReadWrite"
+  disk_size_gb    = 30
+  image_publisher = "Canonical"
+  image_offer     = "0001-com-ubuntu-server-jammy"
+  image_sku       = "22_04-lts-gen2"
   image_version   = "latest"
+
+  enable_disk_encryption_set = true
+  key_vault_id               = module.key_vault.id
   data_disks = [
     {
       name                 = "disk1"
-      disk_size_gb         = 128
+      disk_size_gb         = 60
       storage_account_type = "StandardSSD_LRS"
     }
   ]
-
   # Extension
   extensions = [{
     extension_publisher            = "Microsoft.Azure.ActiveDirectory"
     extension_name                 = "AADLogin"
-    extension_type                 = "AADLoginForWindows"
+    extension_type                 = "AADSSHLoginForLinux"
     extension_type_handler_version = "1.0"
     auto_upgrade_minor_version     = true
     automatic_upgrade_enabled      = false
   }]
+
   #### enable diagnostic setting
-  diagnostic_setting_enable  = false
-  log_analytics_workspace_id = ""
+  diagnostic_setting_enable  = true
+  log_analytics_workspace_id = module.log-analytics.workspace_id ## when diagnostic_setting_enable enable,  add log analytics workspace id
+
+  #vm With User Data
+  user_data = file("user-data.sh")
 }
